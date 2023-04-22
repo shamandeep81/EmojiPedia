95fef039d0cc7ab7548e50011f8feb8854f19ab7
shamandeep81 <126194905+shamandeep81@users.noreply.github.com>
2023-04-22 16:06:01 +0530

    Initial commit

---
+++ src/components/App.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import Card from "./Card";
+import emojipedia from "../emojipedia";
+
+function createEmoji(emojiTerm) {
+  return (
+    <Card
+      key={emojiTerm.id}
+      name={emojiTerm.name}
+      emoji={emojiTerm.emoji}
+      meaning={emojiTerm.meaning}
+    />
+  );
+}
+
+function App() {
+  return (
+    <div>
+      <h1>
+        <span>emojipedia</span>
+      </h1>
+
+      <dl className="dictionary">{emojipedia.map(createEmoji)}</dl>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/emojipedia.js
@@ -0,0 +1,137 @@
+const emojipedia = [
+  {
+    id: 1,
+    emoji: "💪",
+    name: "Tense Biceps",
+    meaning:
+      "“You can do that!” or “I feel strong!” Arm with tense biceps. Also used in connection with doing sports, e.g. at the gym."
+  },
+  {
+    id: 2,
+    emoji: "🙏",
+    name: "Person With Folded Hands",
+    meaning:
+      "Two hands pressed together. Is currently very introverted, saying a prayer, or hoping for enlightenment. Is also used as a “high five” or to say thank you."
+  },
+  {
+    id: 3,
+    emoji: "🤣",
+    name: "Rolling On The Floor, Laughing",
+    meaning:
+      "This is funny! A smiley face, rolling on the floor, laughing. The face is laughing boundlessly. The emoji version of “rofl“. Stands for „rolling on the floor, laughing“."
+  },
+
+  {
+    id: 4,
+    emoji: "😍",
+    name: "Smiling Face With Heart-Eyes",
+    meaning:
+      "A smile of love is shown by the 😍 emoji, which beams with happiness with its heart-shaped eyes. It conveys intense love and affection for a person, place or even an object. This emoji is often used in romantic messages to express love, gratitude, and admiration."
+  },
+
+  {
+    id: 5,
+    emoji: "😮‍💨",
+    name: "Smiley exhaling audibly",
+    meaning:
+      "The emoji exhales noisily. Maybe with relief (phew, that was a close thing!) or with disappointment. There is something we may not like at all. Can also mean that the day was very exhausting and now you feel fatigue."
+  },
+
+  {
+    id: 6,
+    emoji: "😵",
+    name: "Dizzy Face",
+    meaning:
+      "I feel dizzy! Smiley with crossed eyes and open mouth is totally dazed. Is confused and so dizzy it doesn't know which side is up. Also symbolizes strong emotions or drunkenness."
+  },
+
+  {
+    id: 7,
+    emoji: "😵‍💫",
+    name: " Face with spiral eyes",
+    meaning:
+      "The mouth is open, the eyes are spirals. The emoji has circulatory problems and is dizzy. Something or someone has cast a spell over us, we are hypnotized. There may be disorientation, e.g. in a foreign city or in a department store."
+  },
+
+  {
+    id: 8,
+    emoji: "🤐",
+    name: "Zipper-Mouth Face",
+    meaning:
+      "Smiley with zipper instead of a mouth is keeping a secret for itself. Lips are closed or chat partner's lips should remain closed. Can also mean silence because you cannot find the right words."
+  },
+
+  {
+    id: 9,
+    emoji: "🥴",
+    name: "Face with unequal eyes and wavy mouth",
+    meaning:
+      "Eyes half open, half closed, slanted mouth, flushed cheeks. The emoji has a confused facial expression: due to tiredness or the currently confused emotional world. Also known as drunken face."
+  },
+
+  {
+    id: 10,
+    emoji: "🤠",
+    name: "Face With Cowboy Hat",
+    meaning:
+      "Howdy! Typical American smiley with cowboy hat from the Wild West. Stands for freedom, nature, and hard work."
+  },
+
+  {
+    id: 11,
+    emoji: "🥲",
+    name: "Smiling face with tear",
+    meaning:
+      "The 🥲 emoji stands for smiling through the tough times and symbolizes trying to stay positive even in difficult moments. You are smiling through the pain instead of being truly happy. It shows the ability to hide true feelings behind a smile when dealing with problems "
+  },
+
+  {
+    id: 12,
+    emoji: "😕",
+    name: "Confused Face",
+    meaning:
+      "A puzzled face 😕 expresses confusion or disagreement. The eyes are open and the forehead is tilted, as if the cheeks are pressed together or the lips are puckered. It signals uncertainty and dissatisfaction with a situation and shows that something else has been imagined. "
+  },
+
+  {
+    id: 13,
+    emoji: "🙁",
+    name: "Slightly frowning face",
+    meaning:
+      "“I'm sorry to hear that“ or “I'm a little annoyed by what you're saying“. With open eyes and a slightly furrowed brow 🙁 it expresses slight dissatisfaction, concern, disappointment or sadness, but usually less intense than the frown ☹. "
+  },
+
+  {
+    id: 14,
+    emoji: "☹",
+    name: "Frowning Face",
+    meaning:
+      "The frowning face ☹ looks sad with the corners of the mouth hanging down and expresses dissatisfaction or discomfort. It looks joyless and unhappy and can show disapproval of things like the weather, small mistakes, or a person's behavior."
+  },
+
+  {
+    id: 15,
+    emoji: "😣",
+    name: "Suffering Face",
+    meaning:
+      "With narrowed eyes and a slightly furrowed brow, the 😣 emoji is an expression of helplessness and effort. It conveys the feeling of going through a difficult situation and suffering. After a tiring day, it looks like you want to throw in the towel. "
+  },
+
+  {
+    id: 16,
+    emoji: "😫",
+    name: "Tired face",
+    meaning:
+      "A tired emoji 😫 with narrowed eyes, a furrowed brow and an open mouth. It represents tiredness, whether it's from lack of sleep, what's going on around us, or a stressful person or situation. We use this emoji to let others know that we are exhausted and desperately need a break. Similarly, it can be used to express extreme weakness and the urgent need for rest. "
+  },
+
+  {
+    id: 17,
+    emoji: "🥺",
+    name: "Face with begging eyes",
+    meaning:
+      "This 🥺 emoji shows a pleading, submissive expression with big, shiny eyes. It is used to ask for something, like money or to borrow a car. Often, the doggy look is the last weapon you need to get what you want when words are not enough. The emoji can also be used to express affection, admiration or emotion through a loving gesture. "
+  }
+];
+
+export default emojipedia;
